a065c5c0fe97e2bb63f1ba1cd6592b8469b05c54
Ruth Vicuña <ruth.vicuna@sansano.usm.cl>
2020-10-11 20:41:58 -0300

    Update clientes.go

--- Lab 1/clientes.go
+++ Lab 1/clientes.go
@@ -2,65 +2,113 @@
 package main
 
 import (
-	
 	"fmt"
 	"os"
+
 	//"time"
 	"log"
-	
+
 	"encoding/csv"
 
 	//"bufio"
-	"io"
+
+	//csv to json
+	"encoding/json"
 )
 
-func leer(nombre string) *os.File{
-	
+type Retailparcel struct {
+	ID       string `json:"id"`
+	Producto string `json:"producto"`
+	Valor    string `json:"valor"`
+	Tienda   string `json:"tienda"`
+	Destino  string `json:"destino"`
+}
+
+func leer(nombre string) *os.File {
+
 	arch, err := os.Open(nombre)
 
-    if err != nil {
-    	log.Fatalln("No se pudo abrir el archivo" + nombre + "para lectura", err) //chequear si está bien, o es con coma o qué
-    }
+	if err != nil {
+		log.Fatalln("No se pudo abrir el archivo"+nombre+"para lectura", err) //chequear si está bien, o es con coma o qué
+	}
 
-    return arch
+	return arch
 }
 
 func main() {
-    
-    //declarar variable
+
+	//declarar variable
 	//var [nombre] [tipo]
 
 	//time:
 	//time.sleep([segundos] time.Second)
 
-
 	//leer csv
-	pyme_csv := leer("pymes.csv")
-    rPyme := csv.NewReader(pyme_csv)
+	//pyme_csv := leer("pymes.csv")
+	//rPyme := csv.NewReader(pyme_csv)
 
-	retail_csv := leer("retail.csv")
-    rRetail := csv.NewReader(retail_csv)
+	//retail_csv := leer("retail.csv")
+	//rRetail := csv.NewReader(retail_csv)
 
+	//for {
 
-	for {
-		
-		// Lee cada entrada (pedido) del csv
-		pedidoP, err_read := rPyme.Read()
-		
-		if err_read == io.EOF {
-			break
-		}
-		
-		if err_read != nil {
-			log.Fatal(err_read)
-			return
-		}
-		
-		fmt.Printf("%s, %s\n", pedidoP[0], pedidoP[1])
+	// Lee cada entrada (pedido) del csv
+	//	pedidoP, err_read := rPyme.Read()
+
+	//	if err_read == io.EOF {
+	//		break
+	//	}
+
+	//	if err_read != nil {
+	//		log.Fatal(err_read)
+	//		return
+	//	}
+
+	//	fmt.Printf("%s, %s\n", pedidoP[0], pedidoP[1])
+	//}
+
+	csv_file, err := os.Open("retail.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csv_file.Close()
+	r := csv.NewReader(csv_file)
+	records, err := r.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
+	var rparcel Retailparcel
+	var rparcels []Retailparcel
+	for _, rec := range records {
+		rparcel.ID = rec[0]
+		rparcel.Producto = rec[1]
+		rparcel.Valor, _ = rec[2]
+		rparcel.Tienda, _ = rec[3]
+		rparcel.Destino = rec[4]
+		rparcels = append(rparcels, rparcel)
+	}
+	// Convert to JSON
+	json_data, err := json.Marshal(rparcels)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//print json data
+	fmt.Println(string(json_data))
 
-	//cada cierto tiempo (definido por input (creo)) tiene que leer 
+	//create json file
+	json_file, err := os.Create("sample.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer json_file.Close()
+
+	json_file.Write(json_data)
+	json_file.Close()
+
+	//cada cierto tiempo (definido por input (creo)) tiene que leer
 	//una entrada y enviar el pedido a logística
 
 	//realizar conexion gRPC como client y enviar datos
